938b9107ce3d6f759bbca22a179e0d3da83b95ec
wangzhengbo <wangzhengbo1984@hotmail.com>
2017-03-08 16:38:36 +0800

    端口改成3000，加上OpenBrowserPlugin插件。

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const OpenBrowserPlugin = require('open-browser-webpack-plugin');
-const port = 3001;
+const port = 3000;
 
 module.exports = {
     entry: path.resolve(__dirname, 'src/index.js'),
@@ -40,6 +40,9 @@ module.exports = {
             template: 'index.html'
         }),
         new ExtractTextPlugin("styles.css"),
-        new webpack.optimize.UglifyJsPlugin()
+        new webpack.optimize.UglifyJsPlugin(),
+        new OpenBrowserPlugin({
+            url: `http://localhost:${port}`
+        })
     ]
 }
